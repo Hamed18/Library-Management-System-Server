8e1eb0e9ceeed039a90fe65cc2d4e381ddc777bb
hamed18 <hamedcnec@gmail.com>
2024-09-06 05:50:23 +0600

    single data loaded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -42,6 +42,13 @@ async function run() {
 		const result = await cursor.toArray();
 		res.send(result);
 	})
+	// load before update
+	app.get('/allBooks/:id', async(req,res) => {
+		const id = req.params.id;
+		const query = {_id : new ObjectId(id)}
+		const result = await AllBookCollection.findOne(query);
+		res.send(result);
+	 })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
