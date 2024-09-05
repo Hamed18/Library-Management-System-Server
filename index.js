be17c15db60979e37b3c11c5cbca802ce5d504b4
hamed18 <hamedcnec@gmail.com>
2024-09-06 03:45:13 +0600

    CREATE. Add Book UI. Data stored in db

--- index.js
+++ index.js
@@ -26,12 +26,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+	// CREATE: send user input data from server to database. client: Add Spot 
+	const AllBookCollection = client.db("BookSharing").collection("AllBooks");
+	app.post('/addBooks', async(req,res) => {
+		const newBook = req.body;
+		console.log(newBook);
+		const result = await AllBookCollection.insertOne(newBook);
+		res.send(result);
+	})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+  //  await client.close();
   }
 }
 run().catch(console.dir);
